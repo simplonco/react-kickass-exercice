dca1a116f0a1f5c6c4a5224cd120e9ba4b90d0aa
Adamska <adams.roy27@gmail.com>
2017-06-19 16:49:42 +0200

    add dropdown btn with links to forms

--- kickass/src/components/Navbar/Navbar.jsx
+++ kickass/src/components/Navbar/Navbar.jsx
@@ -6,18 +6,27 @@ class Navbar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      hidden: true
+      hiddenNav: true,
+      dropdownBtn: true
     }
   }
 
-  handleToggle = () => {
-    let value = this.state.hidden =! this.state.hidden
-    this.setState({hidden: value})
+  ToggleNavVisibility = () => {
+    let hidden = this.state.hiddenNav =! this.state.hiddenNav
+    this.setState({hiddenNav: hidden})
+  }
+
+  handleDropdownButon = () => {
+    let hidden = this.state.dropdownBtn =! this.state.dropdownBtn;
+    this.setState({dropdownBtn: hidden})
   }
 
   render() {
     const toggleVisibility =
-    this.state.hidden == false ? 'active' : '';
+    this.state.hiddenNav == false ? 'active' : '';
+
+    const dropdownBtn =
+    this.state.dropdownBtn == false ? 'dropdown-list' : '';
 
     return (
       <nav>
@@ -25,8 +34,13 @@ class Navbar extends React.Component {
           <li id="brand"><Link to="/">KickAssStarter</Link></li>
           <li className={toggleVisibility}><Link to="/users">Utilisateurs</Link></li>
           <li className={toggleVisibility}><Link to="/projects">Projets</Link></li>
-          <li className={toggleVisibility}>Ajouts</li>
-          <div id="toggle-nav" onClick={this.handleToggle}>
+          <li id="dropdown-btn" className={toggleVisibility} onClick={this.handleDropdownButon}>Ajouts
+            <ul className={dropdownBtn}>
+              <li className="none"><Link to='/usersForm'>ajouter un utilisateur</Link></li>
+              <li className="none"><Link to='/projectsForm'>ajouter un projet</Link></li>
+            </ul>
+          </li>
+          <div id="toggle-nav" onClick={this.ToggleNavVisibility}>
             <span className="line"></span>
             <span className="line"></span>
             <span className="line"></span>
