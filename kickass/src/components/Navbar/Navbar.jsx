9dda18c3d444a3a6de86b0f746eb267b580a093e
Adamska <adams.roy27@gmail.com>
2017-06-19 15:33:15 +0200

    add toggle navbar on click when reso < 700px

--- kickass/src/components/Navbar/Navbar.jsx
+++ kickass/src/components/Navbar/Navbar.jsx
@@ -2,23 +2,40 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
-const Navbar = () => {
-  return (
-    <nav>
-      <ul className="nav-list">
-        <li id="brand"><Link to="/">KickAssStarter</Link></li>
-        <li><Link to="/users">Utilisateurs</Link></li>
-        <li><Link to="/projects">Projets</Link></li>
-        <li>Ajouts</li>
-          <div id="toggle-nav">
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hidden: true
+    }
+  }
+
+  handleToggle = () => {
+    let value = this.state.hidden =! this.state.hidden
+    this.setState({hidden: value})
+  }
+
+  render() {
+    const toggleVisibility =
+    this.state.hidden == false ? 'active' : '';
+
+    return (
+      <nav>
+        <ul className="nav-list">
+          <li id="brand"><Link to="/">KickAssStarter</Link></li>
+          <li className={toggleVisibility}><Link to="/users">Utilisateurs</Link></li>
+          <li className={toggleVisibility}><Link to="/projects">Projets</Link></li>
+          <li className={toggleVisibility}>Ajouts</li>
+          <div id="toggle-nav" onClick={this.handleToggle}>
             <span className="line"></span>
             <span className="line"></span>
             <span className="line"></span>
             <span className="line"></span>
           </div>
-      </ul>
-    </nav>
-  )
+        </ul>
+      </nav>
+    )
+  }
 }
 
 export default Navbar;
--- kickass/src/views/Home.jsx
+++ kickass/src/views/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../CSS/Home.css';
 
 class Home extends React.Component {
 
